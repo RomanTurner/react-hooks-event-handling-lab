432843c9c38cefe3f2ce843f6e2d52041a758b30
Roman Turner <rn.turner@live.com>
2021-04-04 12:05:06 -0700

    assignment complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function handleFocus(event) {
+    console.log('Good!', event);
+}
+
+function handleBlur(event) {
+    console.log("Hey! Eyes on me!", event);
+}
+
+
+export default function EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} type='button'>Eyes on me</button>
+        </div>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const changeHandler = (event) => {
+    console.log('Entering password...', event.target.value);
+}
+
+export default function Keypad() {
+    return (
+        <div>
+            <input onChange={(e) => changeHandler(e)} type='password'/>
+        </div>
+    )
+}
